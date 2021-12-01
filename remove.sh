88c5bbcbf4a9cd330468a6cbebab0f612234d5b6
Sungrim Lee <91963317+sl0501@users.noreply.github.com>
2021-12-01 17:55:44 -0600

    Delete remove.sh
    expired file

--- remove.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#--remove all directories under current directory except log directories 
-#--remove all files except batch files needed
-find . -mindepth 1 -maxdepth 1 ! \( -name "log" -o -name "output" -o -name "error" \) -type d -exec rm -r {} +
-find . ! \( -name "*.sh" -o -name "*.sub" -o -name "*.dag" \) -type f -exec rm -r {} +
-rm -f sentiment.dag.condor.sub 
